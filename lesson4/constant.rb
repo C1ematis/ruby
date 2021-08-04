5c476f999992a5a03612e558a97174c4893a9d25
EC2 Default User <ec2-user@ip-172-31-47-44.ec2.internal>
2021-08-04 14:03:17 +0000

    Finish: Ruby Lesson4

---
+++ lesson4/constant.rb
@@ -0,0 +1,9 @@
+Pi = 3.14
+puts Pi
+
+Pi = 100
+puts Pi
+#出力結果
+# 3.14
+# constant.rb:4: warning: already initialized constant Pi         ：4行目で初期化された定数Piが使用されています
+# constant.rb:1: warning: previous definition of Pi was here      ：これより前にPiが初期化されているのは1行目になります
