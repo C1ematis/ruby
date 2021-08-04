4f73e9547fa9d950b5527015d796c9de9dbcd83f
EC2 Default User <ec2-user@ip-172-31-47-44.ec2.internal>
2021-08-04 15:00:12 +0000

    Finish: Ruby Lesson6

---
+++ lesson6/apple.rb
@@ -0,0 +1,23 @@
+apple = "Nagano"
+
+if apple == "Aomori"
+  puts "このリンゴは青森産です"
+elsif apple == "Nagano"
+  puts "このリンゴは青森産ではなく、長野産です"
+else
+  puts "このリンゴは青森産でも長野産でもありません"
+end
+# 出力結果
+# このリンゴは青森産ではなく、長野産です
+
+apple = "Yamanashi"
+
+if apple == "Aomori"
+  puts "このリンゴは青森産です"
+elsif apple == "Nagano"
+  puts "このリンゴは青森産ではなく、長野産です"
+else
+  puts "このリンゴは青森産でも長野産でもありません"
+end
+# 出力結果
+# このリンゴは青森産でも長野産でもありまん
