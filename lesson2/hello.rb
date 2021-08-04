3a0644dc666dc87cf514acd80188e407e0f82feb
EC2 Default User <ec2-user@ip-172-31-47-44.ec2.internal>
2021-08-04 13:09:32 +0000

    Finsh: Ruby Lesson2

--- lesson2/hello.rb
+++ lesson2/hello.rb
@@ -1,7 +1,3 @@
 puts 'Hello, World'
-<<<<<<< HEAD
 #出力結果
 #Hello, World
-=======
-#出力：Hello, World
->>>>>>> 7c83a82a67c71f8b3396ce12688dc3280435b687
--- lesson2/name.rb
+++ lesson2/name.rb
@@ -1,7 +1,3 @@
 puts "I" + "am" + "Sam"
-<<<<<<< HEAD
 #出力結果
 #IamSam
-=======
-#出力：IamSam
->>>>>>> 7c83a82a67c71f8b3396ce12688dc3280435b687
