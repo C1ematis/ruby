5755be27485ca9bede80f2f53b2334f456ab80d8
EC2 Default User <ec2-user@ip-172-31-47-44.ec2.internal>
2021-08-04 14:33:36 +0000

    Finish: Ruby Lesson5

---
+++ lesson5/array.rb
@@ -0,0 +1,8 @@
+names = ["Git", "HTML","CSS"]
+puts names[1]
+
+tall = {"太郎"=>185, "次郎"=>170, "花子"=>150}
+puts tall["太郎"]
+# 出力結果
+# HTML
+# 185
---
+++ lesson5/weight.rb
@@ -0,0 +1,9 @@
+name = "A"
+weight = 50
+puts name + "さんの体重は" + weight.to_s + "kgです"
+puts "#{name}さんの体重は#{weight}kgです"
+puts '#{name}さんの体重は#{weight}kgです'
+# 出力結果
+# Aさんの体重は50kgです
+# Aさんの体重は50kgです
+# #{name}さんの体重は#{weight}kgで
